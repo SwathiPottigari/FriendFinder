6340336cfe905a64c0d4e1c5eaa54ca16df6e51d
Swathi <pottigariswathi@gmail.com>
2019-10-04 15:48:15 -0700

    Added api routes for the website

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,16 @@
-var express=require("express");
+var express = require("express");
 var router = express.Router();
-var path=require("path");
+var path = require("path");
 
-router.get("/home",function(req,res){
-res.sendFile(path.join(__dirname,"home.html"));
+// This method redirects to the home page
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname,'../public/home.html'));
+    
 });
+
+// This method redirects to the home page
+router.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+module.exports=router;
--- server.js
+++ server.js
@@ -7,7 +7,15 @@ var PORT=process.env.PORT||3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// This imports the htmlRoutes file
+var htmlRoutes=require("./app/routing/htmlRoutes");
+app.use(htmlRoutes);
 
+// This imports the htmlRoutes file
+var apiRoutes=require("./app/routing/apiRoutes");
+app.use(apiRoutes);
 
 // Starts the server to begin listening
-app.listen(PORT,function(){});
+app.listen(PORT,function(){
+    console.log("listening at "+PORT);
+});
