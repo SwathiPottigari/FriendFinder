c795964089af12910633250521d9ee4c73284a59
Swathi <pottigariswathi@gmail.com>
2019-10-07 14:17:01 -0700

    trying the css file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express=require("express");
 var app=express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app/public'));
 
 var PORT=process.env.PORT||3000;
 
